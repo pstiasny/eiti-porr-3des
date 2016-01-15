3cc5579065ee4d18a86735d2247f6c2a06190d2b
Paweł Stiasny <pawelstiasny@gmail.com>
2016-01-15 03:30:48 +0100

    wczytywanie całego pliku do bufora

--- mainmpi.c
+++ mainmpi.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include "mpi.h"
 
 #include "des.h"
 
 #define BUF_SIZE 256
-const char *USAGE = "Usage: %s <enc|dec> <key1> <key2> <key3>\n";
+const char *USAGE = "Usage: %s <enc|dec> <key1> <key2> <key3> <infile>\n";
 
 int main(int argc, char *argv[]) {
     enum { ENCRYPT, DECRYPT } mode;
-    uint64_t keys[3], in, out;
+    struct stat filestat;
+    uint64_t *buf, out;
+    uint64_t keys[3];
     KS ks[3];
-    int i, j, readc, rank, size;
-    unsigned char cin[BUF_SIZE][8], cout[BUF_SIZE][8];
+    int i, j, readc, rank, size, infile;
+    unsigned char cbuf[8];
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (argc != 5) {
+    if (argc != 6) {
         fprintf(stderr, USAGE, argv[0]);
         return 1;
     }
@@ -37,29 +44,58 @@ int main(int argc, char *argv[]) {
         sscanf(argv[2+i], "%llx", &keys[i]);
         build_KS(keys[i], &ks[i]);
     }
-
-    while (readc = read(0, cin, 8*BUF_SIZE)) {
-        if (readc == 0)
-            return 0;
-        if (readc == -1) {
-            fprintf(stderr, "IO error\n");
+    
+    if (rank == 0) {
+        infile = open(argv[5], O_RDONLY);
+        if (infile < 0) {
+            fprintf(stderr, "couldn't open input file");
             return 1;
         }
-        if (readc % 8) {
+
+        if (0 > fstat(infile, &filestat)) {
+            fprintf(stderr, "couldn't stat input file");
+            return 1;
+        }
+
+        if (filestat.st_size % 8) {
             fprintf(stderr, "Input length not multiple of 8 bytes\n");
             return 1;
         }
 
-        for (j = 0; j < readc / 8; ++j) {
-            in = cblock2uint(cin[j]);
-            if (mode == ENCRYPT)
-                out = des3_encrypt_block(in, ks);
-            else
-                out = des3_decrypt_block(in, ks);
-            uint2cblock(out, cout[j]);
+        buf = (uint64_t*)malloc(filestat.st_size);
+        if (!buf) {
+            fprintf(stderr, "failed to allocate memory\n");
+            return 1;
         }
-        write(1, cout, readc);
+
+        for (j = 0; j < filestat.st_size / 8; ++j) {
+            readc = read(infile, cbuf, 8);
+            if (8 != readc) {
+                fprintf(stderr, "IO error\n");
+                return 1;
+            }
+
+            buf[j] = cblock2uint(cbuf);
+        }
     }
+
+    if (rank != 0) {MPI_Finalize();return 0;} // TODO: send data to workers
+
+    for (j = 0; j < filestat.st_size / 8; ++j) {
+        if (mode == ENCRYPT)
+            out = des3_encrypt_block(buf[j], ks);
+        else
+            out = des3_decrypt_block(buf[j], ks);
+        buf[j] = out;
+    }
+
+    if (rank == 0) {
+        for (j = 0; j < filestat.st_size / 8; ++j) {
+            uint2cblock(buf[j], cbuf);
+            write(1, cbuf, 8);
+        }
+    }
+
     MPI_Finalize();
     return 0;
 }
